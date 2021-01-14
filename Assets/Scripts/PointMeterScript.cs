a0e26d589f3de8669fb4456027198f8386a65918
Roza13-bit <roizamir876@gmail.com>
2021-01-14 16:38:17 +0200

    Gliding Clans V0.2.2 - Tweaked Paint Splatter, Tweaked Camera Positions.
    Gliding Clans V0.2.2 - Tweaked Paint Splatter, Tweaked Camera Positions.

--- Assets/Scripts/PointMeterScript.cs
+++ Assets/Scripts/PointMeterScript.cs
@@ -15,7 +15,7 @@ public class PointMeterScript : MonoBehaviour
 
         splatterQuadGO.SetActive(true);
 
-        splatterQuadGO.transform.localScale = new Vector3(75f * cgsSC.sizeMultiply, 75f * cgsSC.sizeMultiply * 3f, 1f);
+        splatterQuadGO.transform.localScale = new Vector3(75f * (cgsSC.sizeMultiply/1.5f), 75f * cgsSC.sizeMultiply * 2f, 1f);
 
         Destroy(other.gameObject);
         
